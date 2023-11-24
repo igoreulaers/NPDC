905b2efc6bc2fa5ef96b5866eb008d2d9c0ebe44
Igor Eulaers <igor.eulaers@outlook.com>
2023-11-24 14:46:59 +0100

    Add COPOL query

---
+++ Query_COPOL.R
@@ -0,0 +1,120 @@
+# Background process ------------------------------------------------------
+library(jsonlite)
+library(openxlsx)
+library(dplyr)
+
+fieldwork_json = fromJSON("https://v2-api.npolar.no/biology/fielddata/?page=..&includeData=true")
+lab_json = fromJSON("https://v2-api.npolar.no/biology/fielddata/_all_/ecotox/?page=..&includeData=true")
+fieldwork_df = fieldwork_json$items$data
+field_df_flat = flatten(fieldwork_df, recursive = TRUE)
+lab_df = lab_json$items$data
+lab_df_flat = flatten(lab_df, recursive = TRUE)
+lab_df_flat <- lab_df_flat %>% rename(fieldNumberLab = fieldNumber)
+lab_df_flat <- lab_df_flat %>% rename(rightsholderLab = rightsholder)
+lab_df_flat <- lab_df_flat %>% rename(scientificNameLab = scientificName)
+lab_df_flat <- lab_df_flat %>% rename(lifestageLab = lifestage)
+lab_df_flat <- lab_df_flat %>% rename(sexLab = sex)
+lab_df_flat <- lab_df_flat %>% rename(dynamicProperties.matrixLab = dynamicProperties.matrix)
+lab_df_flat <- lab_df_flat %>% rename(dynamicProperties.responsibleLab = dynamicProperties.responsible)
+fieldLab_df <- merge(field_df_flat, lab_df_flat, by = "eventID")
+
+
+# All MOSJ data - simple format -------------------------------------------
+COPOL <- fieldLab_df[fieldLab_df$dynamicProperties.projectName == "COPOL",]
+COPOL <- COPOL[complete.cases(COPOL[ , "eventID"]), ]
+
+oldCOLname_simple <- c("fieldNumber", "scientificName",
+                       "eventDate", "locality",
+                       "dynamicProperties.matrix", "measurementUnit", "dynamicProperties.fatPercentage",
+                       "dynamicProperties.measurementCategory", "measurementType", "measurementValue")
+newCOLname_simple <- c("ID.field", "species",
+                       "date_field", "locality",
+                       "matrix", "unit", "EOM.%",
+                       "group", "compound", "concentration")
+COPOL_long_simple <- COPOL %>% select(8, 13,
+                                    6, 10,
+                                    19, 46, 54,
+                                    56, 45, 47) %>% rename_with(~ newCOLname_simple, all_of(oldCOLname_simple))
+
+write.xlsx(COPOL_long_simple, file = "COPOL - All data - simple.xlsx")
+
+# All COPOL data - extended field format -----------------------------------------
+COPOL <- fieldLab_df[fieldLab_df$dynamicProperties.projectName == "COPOL",]
+COPOL <- COPOL[complete.cases(COPOL[ , "eventID"]), ]
+
+oldCOLname_extF <- c("fieldNumber", "scientificName",
+                     "eventDate", "locality", "decimalLatitude", "decimalLongitude",
+                     "lifestage", "dynamicProperties.age", "sex", "dynamicProperties.weightInGrams",
+                     "rightsholder", "dynamicProperties.responsible", "samplingProtocol",
+                     "dynamicProperties.matrix", "measurementUnit", "dynamicProperties.fatPercentage",
+                     "dynamicProperties.measurementCategory", "measurementType", "measurementValue")
+newCOLname_extF <- c("ID.field", "species",
+                     "date_field", "locality", "latitude", "longitude",
+                     "maturity", "age", "sex", "mass.gram",
+                     "rightsholder_field", "responsible_field", "protocol_field",
+                     "matrix", "unit", "EOM.%",
+                     "group", "compound", "concentration")
+COPOL_long_extF <- COPOL %>% select(8, 13,
+                                  6, 10, 4, 5, 
+                                  15, 18, 17, 27,
+                                  12, 21, 16,
+                                  19, 46, 54,
+                                  56, 45, 47) %>% rename_with(~ newCOLname_extF, all_of(oldCOLname_extF))
+
+write.xlsx(COPOL_long_extF, file = "COPOL - All data - extended field.xlsx")
+
+# All COPOL data - extended lab format -------------------------------------
+COPOL <- fieldLab_df[fieldLab_df$dynamicProperties.projectName == "COPOL",]
+COPOL <- COPOL[complete.cases(COPOL[ , "eventID"]), ]
+
+oldCOLname_extL <- c("fieldNumber", "scientificName",
+                     "eventDate", "locality",
+                     "dynamicProperties.matrix", "measurementUnit", "dynamicProperties.fatPercentage",
+                     "dynamicProperties.measurementCategory", "measurementType", "measurementValue",
+                     "rightsholderLab", "measurementDeterminedBy", "dynamicProperties.responsibleLab", "measurementDeterminedDate",
+                     "dynamicProperties.detectionLimit", "dynamicProperties.levelOfQuantification", "dynamicProperties.percentRecovery")
+newCOLname_extL <- c("ID.field", "species",
+                     "date_field", "locality",
+                     "matrix", "unit", "EOM.%",
+                     "group", "compound", "concentration",
+                     "rightsholder_lab", "lab", "responsible_lab", "date_lab",
+                     "LOD", "LOQ", "recovery")
+COPOL_long_extL <- COPOL %>% select(8, 13,
+                                  6, 10,
+                                  19, 46, 54,
+                                  56, 45, 47,
+                                  49, 41, 58, 42,
+                                  53, 61, 60) %>% rename_with(~ newCOLname_extL, all_of(oldCOLname_extL))
+
+write.xlsx(COPOL_long_extL, file = "COPOL - All data - extended lab.xlsx")
+
+# All COPOL data - extended field and lab format ---------------------------
+COPOL <- fieldLab_df[fieldLab_df$dynamicProperties.projectName == "COPOL",]
+COPOL <- COPOL[complete.cases(COPOL[ , "eventID"]), ]
+
+oldCOLname_extFL <- c("fieldNumber", "scientificName",
+                      "eventDate", "locality", "decimalLatitude", "decimalLongitude",
+                      "lifestage", "dynamicProperties.age", "sex", "dynamicProperties.weightInGrams",
+                      "rightsholder", "dynamicProperties.responsible", "samplingProtocol",
+                      "dynamicProperties.matrix", "measurementUnit", "dynamicProperties.fatPercentage",
+                      "dynamicProperties.measurementCategory", "measurementType", "measurementValue",
+                      "rightsholderLab", "measurementDeterminedBy", "dynamicProperties.responsibleLab", "measurementDeterminedDate",
+                      "dynamicProperties.detectionLimit", "dynamicProperties.levelOfQuantification", "dynamicProperties.percentRecovery")
+newCOLname_extFL <- c("ID.field", "species",
+                      "date_field", "locality", "latitude", "longitude",
+                      "maturity", "age", "sex", "mass.gram",
+                      "rightsholder_field", "responsible_field", "protocol_field",
+                      "matrix", "unit", "EOM.%",
+                      "group", "compound", "concentration",
+                      "rightsholder_lab", "lab", "responsible_lab", "date_lab",
+                      "LOD", "LOQ", "recovery")
+COPOL_long_extFL <- COPOL %>% select(8, 13,
+                                   6, 10, 4, 5, 
+                                   15, 18, 17, 27,
+                                   12, 21, 16,
+                                   19, 46, 54,
+                                   56, 45, 47,
+                                   49, 41, 58, 42,
+                                   53, 61, 60) %>% rename_with(~ newCOLname_extFL, all_of(oldCOLname_extFL))
+
+write.xlsx(COPOL_long_extFL, file = "COPOL - All data - extended field and lab.xlsx")
